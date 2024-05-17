e2d95dccf8401d729d1faf3929492190fc98f5e6
IsaiasNero7 <127417946+IsaiasNero7@users.noreply.github.com>
2024-05-17 08:27:31 -0300

    f23rf4

---
+++ Menu.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById('menu')
+const btn = document.getElementById('btn-menu') 
+const treino = document.getElementById('treino') 
+
+
+function animar() {
+  
+  menu.classList.toggle('abrir')
+  
+}
