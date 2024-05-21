639d79841e61960551dd5278f7fe087120348d37
IsaiasNero7 <127417946+IsaiasNero7@users.noreply.github.com>
2024-05-21 10:56:42 -0300

    conexao bd

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php 
+$host="localhost";
+$user="root";
+$sen="";
+$bd="academia";
+
+$conexao=mysqli_connect($host, $user, $sen, $bd);
+//if(!$conexao){
+ //   echo"Email ou senha incorreto: ".mysqli_connect_error();
+//}
+//else{
+ //   echo"deu certo";
+//}
+?>
---
+++ login.php
@@ -0,0 +1,18 @@
+<?php 
+
+include("conexao.php");
+
+$email= $_POST['email'];
+$senha= $_POST['senha'];
+
+$sql= "INSERT INTO loginl (email,senha)
+value ('$email','$senha')";
+if(mysqli_query($conexao,$sql)){
+    echo "usuario cadastrado com sucesso";
+}
+else{
+    echo"erro" . mysqli_connect_error($conexao);
+}
+  
+
+?>
