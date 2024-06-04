f1e9180d80be5a4c1fc2cceb2ed449a24021357e
IsaiasNero7 <127417946+IsaiasNero7@users.noreply.github.com>
2024-06-04 08:58:07 -0300

    sofrimento...

--- Menu.js
+++ Menu.js
@@ -1,10 +1,36 @@
-const menu = document.getElementById('menu')
-const btn = document.getElementById('btn-menu') 
-const treino = document.getElementById('treino') 
+const menu = document.getElementById('dropdown')
+const btn = document.getElementById('btn')
 
+menu.addEventListener('click', animar)
 
 function animar() {
-  
+
   menu.classList.toggle('abrir')
+
+
+}
+
+
+
+
+const menu_mobile = document.getElementById('menu')
+const btn_menu = document.getElementById('btn-menu') 
+menu_mobile.addEventListener('click', animar)
+
+function anima() {
+  
+  menu_mobile.classList.toggle('abrir')
+  btn_menu.classList.toggle('ativar')
   
 }
+
+const menu_t = document.getElementById('dropdane-m')
+const btn_t = document.getElementById('btn-treino') 
+menu_t.addEventListener('click', animar)
+
+function anim() {
+  
+  btn_t.classList.toggle('abri')
+  
+}
+
